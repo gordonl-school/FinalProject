12c0b33f124c42ef0f346059f71e919658171efa
BT_1N3_03 <jenniferchenxil2789@nycstudents.net>
2025-05-27 14:01:17 -0400

    started work for animation

--- src/Player.java
+++ src/Player.java
@@ -2,14 +2,23 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Player {
     private final int MOVE_AMOUNT = 3;
     private BufferedImage image;
-
+    private Animation animationDown;
+    private Animation animationTop;
+    private Animation animationSide;
+    private Animation currentAnimation;
+    private boolean facingRight;
+    private boolean facingForward;
     private int xCoord;
     private int yCoord;
+
     public Player() {
+        facingForward = true;
+        facingRight = false;
         xCoord = 500;
         yCoord = 500;
         try {
@@ -17,6 +26,18 @@ public class Player {
         } catch (IOException error) {
             System.out.println(error.getMessage());
         }
+
+        ArrayList<BufferedImage> images = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            String fileName = "src\\tile00" + i + ".png";
+            try {
+                images.add(ImageIO.read(new File(fileName)));
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        ///working on adding animatoin
     }
 
     // Key Interactions
